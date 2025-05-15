fb9b592dbc8ebcaf23bc378d974d102d5ec4d239
JohnSheppard7901 <john.sheppard7901@gmail.com>
2025-05-15 14:42:45 +0200

    RegisteredUser Entity created

---
+++ Backend/src/main/java/at/technikum/backend/entity/RegisteredUser.java
@@ -0,0 +1,58 @@
+package at.technikum.backend.entity;
+
+public class RegisteredUser {
+
+    private char sex;   //char: w,m,d keine anderen werden akzeptiert
+    private String username;
+    private String email;
+    private String password;
+    private String land;
+
+    public RegisteredUser(char sex, String username, String email, String password, String land){
+        this.sex = sex;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.land = land;
+    }
+
+
+    //GETTERS AND SETTERS
+    public char getSex(){
+        return this.sex;
+    }
+    public void setSex(char sex){
+        this.sex = sex;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+    public void setUsername(){
+        this.username = username;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(){
+        this.password = password;
+    }
+
+    public String getLand(){
+        return this.land;
+    }
+    public void setLand(){
+        this.land = land;
+    }
+
+
+
+}
