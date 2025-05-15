9c88079f218533096d23b7dca613cb2f4301c98a
sh <hauss.sebastian@proton.me>
2025-05-15 13:13:36 +0200

    backend base structure (Luisa)

---
+++ Backend/src/main/java/at/technikum/backend/controller/ArticleController.java
@@ -0,0 +1,48 @@
+package at.technikum.backend.controller;
+
+import at.technikum.backend.entity.Article;
+import at.technikum.backend.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class ArticleController {
+
+    private ArticleService articleService = new ArticleService();
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+
+    @GetMapping("article")
+    public List<Article> readAll() {
+        return articleService.getAllArticle();
+    }
+
+    @GetMapping("/{id}")
+    public Article read(@PathVariable int id) {
+        return articleService.getArticle(id);
+    }
+
+    @PostMapping
+    public void addArticle(@RequestBody Article article) {
+        articleService.addArticle(article);
+    }
+
+    @PutMapping
+    public void updateArticle(@RequestBody Article article, @RequestBody String newText) {
+        articleService.updateArticle(article, newText);
+    }
+
+    @DeleteMapping
+    public void deleteArticle(@RequestBody Article article) {
+        articleService.deleteArticle(article);
+    }
+
+
+}
