995497817d61567ef17535d102f8d660fd2d51ea
JohnSheppard7901 <john.sheppard7901@gmail.com>
2025-05-27 15:54:59 +0200

    Controller und Service für Profile

---
+++ Backend/src/main/java/at/technikum/backend/repository/ProfileRepository.java
@@ -0,0 +1,10 @@
+package at.technikum.backend.repository;
+
+import at.technikum.backend.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, String> {
+
+}
--- Backend/src/main/java/at/technikum/backend/service/ProfileService.java
+++ Backend/src/main/java/at/technikum/backend/service/ProfileService.java
@@ -1,4 +1,57 @@
 package at.technikum.backend.service;
 
+import at.technikum.backend.entity.Profile;
+import at.technikum.backend.exceptions.ProfileAlreadyExistsException;
+import at.technikum.backend.exceptions.ProfileNotFoundException;
+import at.technikum.backend.repository.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProfileService {
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository){
+        this.profileRepository = profileRepository;
+    }
+
+    public Profile create(Profile profile){
+        if (checkIfProfileExists(profile.getId()).isPresent()){
+            throw new ProfileAlreadyExistsException("Profile already exists.");
+        }
+        return profileRepository.save(profile);
+    }
+
+    public List<Profile> readAll(){
+        return profileRepository.findAll();
+    }
+
+    public Profile read(String id){
+        if (checkIfProfileExists(id).isEmpty()){
+            throw new ProfileNotFoundException("Profile not found.");
+        }
+        return checkIfProfileExists(id).get();
+    }
+
+    public Profile update(Profile profile){
+        if (checkIfProfileExists(profile.getId()).isEmpty()){
+            throw new ProfileNotFoundException("Profile not found.");
+        }
+        return profileRepository.save(profile);
+    }
+
+    public void delete(String id){
+        if (checkIfProfileExists(id).isEmpty()){
+            throw new ProfileNotFoundException("Profile not found.");
+        }
+        profileRepository.delete(checkIfProfileExists(id).get());
+    }
+
+
+
+    public Optional<Profile> checkIfProfileExists(String id){
+        return profileRepository.findById(id);
+    }
 }
