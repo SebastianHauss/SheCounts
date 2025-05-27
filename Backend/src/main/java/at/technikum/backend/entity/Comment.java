f5a4ed743bddb85c40ead2afb06decc56e8f07fb
JohnSheppard7901 <john.sheppard7901@gmail.com>
2025-05-27 08:57:16 +0200

    Comment Entity erstellt

---
+++ Backend/src/main/java/at/technikum/backend/entity/Comment.java
@@ -0,0 +1,64 @@
+package at.technikum.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    /*
+    Todo: verschachtelte Comments recherche
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    //@ForeignKey(Article)
+    private String article_id;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+
+
+
+    public Comment(String article_id, String content, String author){
+        this.article_id = article_id;
+        this.content = content;
+        this.author = author;
+
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Comment() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getArticle_id() {
+        return article_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
