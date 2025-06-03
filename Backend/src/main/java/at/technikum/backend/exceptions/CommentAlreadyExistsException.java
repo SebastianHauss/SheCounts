21d0cae63a2274cda661bfbfb10d1d1f18c7007c
sh <hauss.sebastian@proton.me>
2025-06-03 13:17:02 +0200

    Status Codes bei CommentExceptions

--- Backend/src/main/java/at/technikum/backend/exceptions/CommentAlreadyExistsException.java
+++ Backend/src/main/java/at/technikum/backend/exceptions/CommentAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package at.technikum.backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class CommentAlreadyExistsException extends RuntimeException {
     public CommentAlreadyExistsException(String message) {
         super(message);
