a786a17d6aeada1b239ff778d4b81c7647797ce0
hauss.sebastian@proton.me <hauss.sebastian@proton.me>
2025-10-03 13:35:05 +0200

    put hashed password into userRepo

--- Backend/src/main/java/at/technikum/backend/UserConfig.java
+++ Backend/src/main/java/at/technikum/backend/UserConfig.java
@@ -5,6 +5,7 @@ import at.technikum.backend.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -12,19 +13,18 @@ import java.util.List;
 public class UserConfig {
 
     @Bean
-    CommandLineRunner userInitializer(UserRepository userRepository) {
+    CommandLineRunner userInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (userRepository.count() == 0) {
-                User admin = new User("admin", "admin@shecounts.at", "admin123");
+                User admin = new User("admin", "admin@shecounts.at", passwordEncoder.encode("admin123"));
                 admin.setAdmin(true);
 
-                User user1 = new User("emily", "emily@example.com", "emily123");
-                User user2 = new User("alex", "alex@example.com", "alex123");
+                User user1 = new User("emily", "emily@example.com", passwordEncoder.encode("emily123"));
+                User user2 = new User("alex", "alex@example.com", passwordEncoder.encode("alex123"));
 
                 userRepository.saveAll(List.of(admin, user1, user2));
             }
         };
     }
-
 
 }
