ac29cc9420e9d21ef1238c84d9149d08b611d87c
verenx <verenx@gmail.com>
2025-10-03 11:41:40 +0200

    adapt WebConfig so any localhost port has access

--- Backend/src/main/java/at/technikum/backend/config/WebConfig.java
+++ Backend/src/main/java/at/technikum/backend/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63343")
+                        .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
