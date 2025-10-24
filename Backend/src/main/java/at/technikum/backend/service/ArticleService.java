fa118b6e0e290ca068c8336c6b8118bbefc641ee
verenx <verenx@gmail.com>
2025-10-24 11:22:59 +0200

    delete import

--- Backend/src/main/java/at/technikum/backend/service/ArticleService.java
+++ Backend/src/main/java/at/technikum/backend/service/ArticleService.java
@@ -10,7 +10,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
