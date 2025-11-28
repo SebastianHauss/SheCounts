f2c1ee2854725173c4ea60fadc4f39d61ead1d89
tklnsk <kalinovskatetiana@gmail.com>
2025-11-28 11:51:10 +0100

    feature/comment Frontend service

--- Frontend/scripts/common/comments.js
+++ Frontend/scripts/common/comments.js
@@ -7,19 +7,78 @@ function getAllComments() {
     contentType: 'application/json',
     crossDomain: true,
     success: function (data) {
-      console.log('Successfully loaded all comments');
+      console.log('Success:', data);
     },
+    error: function (xhr, status, error) {
+      console.log('Error:', error);
+    }
   });
 }
 
 function getComment(id) {
   $.ajax({
     url: `${API_URL}/${id}`,
-    type: 'GET',
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      console.log('Success:', data);
+    },
+    error: function (xhr, status, error) {
+      console.log('Error:', error);
+    }
+  });
+}
+
+
+function createComment() {
+  const title = $('#title-input').val().trim();
+  const text =  $('#text-area').val().trim();
+
+  // TODO VALIDATE
+
+  $.ajax({
+    url: `${API_URL}/`,
+    type: 'POST',
     contentType: 'application/json',
     crossDomain: true,
+    data:JSON.stringify({title, text}),
     success: function (data) {
-      console.log('Successfully loaded all comments');
+      console.log('Success:', data);
     },
+    error: function (xhr, status, error) {
+      console.log('Error:', error);
+    }
   });
+
+  //UPDATE
+  function updateComment(id) {
+    $.ajax({
+      url: `${API_URL}/${id}`,
+      type: 'PUT',
+      contentType: 'application/json',
+      crossDomain: true,
+      data: JSON.stringify({title, text}),
+      success: function (data) {
+        console.log('Success:', data);
+      },
+      error: function (xhr, status, error) {
+        console.log('Error:', error);
+      }
+    });
+  }
+  //DELETE
+  function deleteComment(id) {
+    $.ajax({
+      url: `${API_URL}/${id}`,
+      type: 'DELETE',
+      contentType: 'application/json',
+      crossDomain: true,
+      success: function (data) {
+        console.log('Success:', data);
+      },
+      error: function (xhr, status, error) {
+        console.log('Error:', error);
+      }
+    });
+  }
 }
