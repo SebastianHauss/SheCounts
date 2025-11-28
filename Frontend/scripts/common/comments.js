16463cc981e9e888be552446925f34eaddb716bd
Sebastian Hauss <hauss.sebastian@proton.me>
2025-11-28 11:06:59 +0100

    Put methods outside of document.ready (not need)

--- Frontend/scripts/common/comments.js
+++ Frontend/scripts/common/comments.js
@@ -1,30 +1,25 @@
 const API_URL = 'http://localhost:8080/api/comments';
 
-$(document).ready(function () {
+function getAllComments() {
+  $.ajax({
+    url: `${API_URL}`,
+    type: 'GET',
+    contentType: 'application/json',
+    crossDomain: true,
+    success: function (data) {
+      console.log('Successfully loaded all comments');
+    },
+  });
+}
 
-  function getAllComments() {
-    $.ajax({
-      url: `${API_URL}`,
-      type: 'GET',
-      contentType: 'application/json',
-      crossDomain: true,
-      success: function (data) {
-        console.log('Successfully loaded all comments');
-      },
-    });
-  }
-
-    function getComment(id) {
-    $.ajax({
-      url: `${API_URL}/${id}`,
-      type: 'GET',
-      contentType: 'application/json',
-      crossDomain: true,
-      success: function (data) {
-        console.log('Successfully loaded all comments');
-      },
-    });
-  }
-  
-
-});
+function getComment(id) {
+  $.ajax({
+    url: `${API_URL}/${id}`,
+    type: 'GET',
+    contentType: 'application/json',
+    crossDomain: true,
+    success: function (data) {
+      console.log('Successfully loaded all comments');
+    },
+  });
+}
