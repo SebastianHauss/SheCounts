e9bb6ba2e6c7569921d982bfd6e8e69ea5dcfcb7
Sebastian Hauss <hauss.sebastian@proton.me>
2025-12-05 21:59:16 +0100

    Implement initial backend for comments

--- Backend/src/main/java/at/technikum/backend/repository/CommentRepository.java
+++ Backend/src/main/java/at/technikum/backend/repository/CommentRepository.java
@@ -4,8 +4,13 @@ import at.technikum.backend.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+
+    List<Comment> findByArticleId(UUID articleId);
+    
+    List<Comment> findByUserId(UUID userId);
 }
