ba0fe71b38017d1ddb2a2a4558688f0bd3c5b9c2
sh <SebastianHauss@users.noreply.github.com>
2026-01-08 19:45:28 +0100

    Add  initBucket method to MinioStorage to create bucket automatically

--- Backend/src/main/java/at/technikum/backend/storage/MinioStorage.java
+++ Backend/src/main/java/at/technikum/backend/storage/MinioStorage.java
@@ -1,10 +1,14 @@
 package at.technikum.backend.storage;
 
 import at.technikum.backend.storage.properties.MinioProperties;
+import io.minio.BucketExistsArgs;
 import io.minio.GetObjectArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +18,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MinioStorage implements FileStorage {
 
     private final MinioProperties minioProperties;
@@ -48,6 +53,26 @@ public class MinioStorage implements FileStorage {
                             .build());
         } catch (Exception e) {
             throw new RuntimeException("Load file failed for file with external id=" + id, e);
+        }
+    }
+
+    @PostConstruct
+    public void initBucket() {
+        try {
+            boolean exists = minioClient.bucketExists(
+                    BucketExistsArgs.builder()
+                            .bucket(minioProperties.getBucket())
+                            .build());
+
+            if (!exists) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder()
+                                .bucket(minioProperties.getBucket())
+                                .build());
+                log.info("MinIO bucket '{}' created", minioProperties.getBucket());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize MinIO bucket", e);
         }
     }
 }
