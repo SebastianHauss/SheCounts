b2aa0a63247549d62ed47b710b68acc06933b337
verenx <verenx@gmail.com>
2026-02-24 10:43:25 +0100

    adapt security config

--- Backend/src/main/java/at/technikum/backend/security/SecurityConfig.java
+++ Backend/src/main/java/at/technikum/backend/security/SecurityConfig.java
@@ -65,7 +65,8 @@ public class SecurityConfig {
                 "http://localhost:5500", 
                 "http://127.0.0.1:5500", 
                 "http://localhost:63342", 
-                "http://localhost:63343"
+                "http://localhost:63343",
+                "http://10.191.28.177:3000"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
